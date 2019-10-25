19fecd3174b9a9f38586fafa65feda4766854a5d
Stefan Müller <St.Mueller@hu-berlin.de>
2019-10-25 19:35:56 +0200

    prepub stuff

--- chopchapters.sh
+++ chopchapters.sh
@@ -1,5 +1,7 @@
 chapternumber=0 
 offset=$1
+dir=$2
+main=$3
 offset=$(($offset-1))
 old=1
 prefix="0"
@@ -11,7 +13,7 @@ if [ $chapternumber -eq 10 ]; then
 fi
 echo $prefix$chapternumber $old-$(($new-1))
 
-pdftk main.pdf cat $old-$(($new-1)) output chapters-pdfs/$prefix$chapternumber.pdf 
+pdftk $main.pdf cat $old-$(($new-1)) output $2-pdfs/$prefix$chapternumber.pdf 
 old=$new
 chapternumber=$(($chapternumber+1))
 done
